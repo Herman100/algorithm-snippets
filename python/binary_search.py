3ca99d35e7d9a7c684904b4fb069a213d779ff25
Herman Kwamebour <hermankwamebour30@gmail.com>
2026-02-17 11:45:49 +0000

    feat: implement binary search algorithm with example usage

---
+++ python/binary_search.py
@@ -0,0 +1,34 @@
+def binary_search(arr, item):
+    low = 0
+    high = len(arr) - 1
+
+    while low<= high:
+        mid = (low + high)//2
+        guess  = arr[mid]
+            
+        if item == guess:
+            return mid
+        
+        if item > guess:
+            low = mid + 1 
+        
+        else:
+            high = mid - 1
+
+    
+           
+    return None
+
+
+
+
+
+
+
+
+
+
+
+my_list = [1, 3, 5, 7, 9] 
+print(binary_search(my_list, 3)) # => 1      
+print(binary_search(my_list, -1)) # => None 
